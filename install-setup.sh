23ead948fced3b7be3d65ce2b2f7318076856c2e
rhfreakytux <132640896+rhfreakytux@users.noreply.github.com>
2024-05-08 10:04:40 +0545

    update: script in the install-setup.sh

---
+++ install-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#date: May 8, 2024
+#author: rhfreakytux
+
+PARENTDIR=development
+NAME=ansible
+DIR=/home/$USER/$PARENTDIR/$NAME
+VENV=.activate-ansible
+
+echo $PASS | sudo -S -E useradd -G wheel $USER && \
+echo $PASS | sudo -S -E echo $USER 'ALL=(ALL) NOPASSWD: ALL' | \
+sudo -S -E tee -a /etc/sudoers && exit
+
+
+echo $PASS | sudo -S -E dnf install git, python3-pip -y
+
+ansible_dev(){
+        mkdir -p $DIR
+        python3 -m venv $DIR/$VENV
+        source $DIR/$VENV/bin/activate && python3 -m pip install --upgrade pip > /dev/null
+        pip3 install ansible-dev-tools > /dev/null
+        ansible-playbook main.yml
+}
+
+ansible_dev
