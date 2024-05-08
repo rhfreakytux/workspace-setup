96d5c948dd8554f44e8b1712016e04372b78d972
rhfreakytux <132640896+rhfreakytux@users.noreply.github.com>
2024-05-09 00:56:22 +0545

    update: task execution after the preceded task is completed and remove sudo standard input password

--- install-setup.sh
+++ install-setup.sh
@@ -7,15 +7,22 @@ FOLDERNAME=ansible
 DIR=/home/$USER/$PARENTDIR/$FOLDERNAME
 VENV=.activate-ansible
 
-echo $PASS | sudo -S -E dnf install git, python3-pip -y
+sudo dnf install git python3-pip -y
 
 ansible_dev(){
         mkdir -p $DIR
+
         python3 -m venv $DIR/$VENV
-        source $DIR/$VENV/bin/activate && python3 -m pip install --upgrade pip > /dev/null
-        pip3 install ansible-dev-tools > /dev/null
-        ansible-playbook main.yml 
-        deactivate
-        echo "You setup is now completed"
+
+        source $DIR/$VENV/bin/activate && python3 -m pip install --upgrade pip > /dev/null && 
+
+        pip3 install ansible-dev-tools > /dev/null &&
+
+        ansible-galaxy collection install community.general > /dev/null &&
+
+        ansible-playbook -K main.yml &&
+
+        deactivate && echo "You setup is now completed"
 }
+
 ansible_dev
