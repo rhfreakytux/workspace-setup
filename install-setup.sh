f5bfd4dadbab0634c7267e0ea0c431d803cd67c1
rhfreakytux <132640896+rhfreakytux@users.noreply.github.com>
2024-05-08 23:39:56 +0545

    added deactivate and echo to the installation script

--- install-setup.sh
+++ install-setup.sh
@@ -14,7 +14,8 @@ ansible_dev(){
         python3 -m venv $DIR/$VENV
         source $DIR/$VENV/bin/activate && python3 -m pip install --upgrade pip > /dev/null
         pip3 install ansible-dev-tools > /dev/null
-        ansible-playbook main.yml
+        ansible-playbook main.yml 
+        deactivate
+        echo "You setup is now completed"
 }
-
 ansible_dev
